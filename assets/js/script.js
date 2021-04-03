1c089996789995f3702a9793a260d537e5a7634e
Punkinut <heypunkinut@gmail.com>
2021-04-04 09:48:49 +1000

    Fixed search page bug

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,10 +12,14 @@ search.on("click", function (event) {
     event.preventDefault();
     if(!searchInput.val() || category.val() === "no") {
         window.alert("Please fill out the required items")
+    } else if (location.pathname === "/search-results.html") {
+        searchInput.attr("Value", "It Works")
     } else {
         location.replace("search-results.html")
     }
 })
+
+// The button that takes you back to the search page
 
 // Function that replaces the url
 function replaceUrl () {
