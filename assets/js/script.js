130b7722cb4e9d00eb0042d2dba427a70bbd4347
Punkinut <heypunkinut@gmail.com>
2021-04-04 09:50:53 +1000

    Quick bug changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,7 +12,7 @@ search.on("click", function (event) {
     event.preventDefault();
     if(!searchInput.val() || category.val() === "no") {
         window.alert("Please fill out the required items")
-    } else if (location.pathname === "/search-results.html") {
+    } else if (location.pathname === "/congress-search-tool/search-results.html") {
         searchInput.attr("Value", "It Works")
     } else {
         location.replace("search-results.html")
