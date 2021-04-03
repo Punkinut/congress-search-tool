60a4c46f218ae68b0ee141a85d4c09bd1d16605a
Punkinut <heypunkinut@gmail.com>
2021-04-03 10:37:29 +1000

    Added the form validation in JS

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+// This is the value of the search input
+var searchInput = $(".search-input");
+
+// This is the value of the category input
+var category = $(".category");
+
+// This is the search button
+var search = $(".search-button");
+
+// This is the form validation event listener
+search.on("click", function (event) {
+    event.preventDefault();
+    if(!searchInput.val() || category.val() === "no") {
+        window.alert("Please fill out the required items")
+    }
+})
+
+
